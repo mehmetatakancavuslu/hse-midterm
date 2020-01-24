9768ae18708b576b8cb053bce4ef0cc7d1dab7a1
mehmetatakancavuslu <mcavuslu15@ku.edu.tr>
2020-01-24 23:08:28 +0300

    Initiate basic model

---
+++ basic_model.R
@@ -0,0 +1,5 @@
+## Basic Model with variables: Treatment, Period2
+## Which will be used on the model of "Amount Spend on Day"
+## Amount spent on day i=b0+b1*period2+b2*treatment+b3*(period2*treatment)+cv
+## Cv(Control Variables): Frequencies/amounts purchases/average basket size/etc
+library(completejourney)
